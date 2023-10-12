f41da8449e1634e25efe41fcd790995e4da631d6
Ravindu Chathuranga <ravicdasanayaka@gmail.com>
2023-10-12 10:05:57 +0530

    initial commit

---
+++ signIn.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="row p-5">
+        <div class="col-12 d-grid justify-content-center">
+            <h3>Sign In</h3>
+            <label for="">Username</label>
+            <input type="text" name="" id="">
+            <label for="">Password</label>
+            <input type="password">
+            <div class="input-group mb-1">
+                <div class="input-group-text">
+                    <span><input class="form-check-input mt-1" type="checkbox" value="" aria-label="Checkbox for following text input me-4">Remeber Me</span>
+                </div>
+            </div>
+            <a href="" class="text-danger">forgot password</a>
+            <button class="btn btn-primary mt-1">Sign In</button>
+        </div>
+
+    </div>
+</body>
+
+</html>
---
+++ signUp.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="row p-5">
+        <div class="col-12 d-grid justify-content-center">
+            <h3>Sign Up</h3>
+            <label for="">first name</label>
+            <input type="text">
+            <label for="">last name</label>
+            <input type="text">
+            <label for="">mobile</label>
+            <input type="text">
+            <label for="">Gender</label>
+            <select name="" id="">
+                <option value="">Male</option>
+                <option value="">Female</option>
+                <option value="">Shemale</option>
+            </select>
+            <label for="">Email</label>
+            <input type="email">
+            <label for="">password</label>
+            <input type="password">
+            <button class="btn btn-primary mt-2">Sign Up</button>
+        </div>
+    </div>
+</body>
+
+</html>
