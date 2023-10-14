ab996703446265c5cd5e864dd0d3064c0a23ab6c
Ravindu Chathuranga <ravicdasanayaka@gmail.com>
2023-10-14 21:50:52 +0530

    added sign In fuction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-function signIn(){
-    var user=document.getElementById("username");
-    var passwrod=document.getElementById("password");
-    
+function signIn() {
+    var user = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var rememberMe = document.getElementById("rememberme").checked;
+
+    var form = new FormData();
+
+    form.append("user", user);
+    form.append("password",password);
+    form.append("rememberMe",rememberMe);
+
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+        if (r.readyState == 4 && r.status == 200) {
+            var text = r.responseText;
+            if(text=="success"){
+                window.location="index.php"
+            }else{
+                alert(text);
+            }
+        }
+    }
+    r.open("POST", "signInprocess.php", true);
+    r.send(form);
 }
